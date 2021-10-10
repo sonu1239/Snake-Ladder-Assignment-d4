bea6825cfd4f80119a438a6ed5ab380967b23f0a
sonu1239 <ss316742@gmail.com>
2021-10-10 22:47:49 +0530

    Rolling dice to snake & Ladder g

--- Program.cs
+++ Program.cs
@@ -10,12 +10,17 @@ namespace SnekeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the snake and ladder game");
+            Console.WriteLine("Welcome to the snake and ladder game.");
             Console.WriteLine();
             Console.WriteLine("Player mode:single: Starting position at 0");
             Console.WriteLine("*Start Game*");
+
+            //Variables
             int position = 0;
-            int dice;
+
+            Random die = new Random();
+            int dice = die.Next(1, 7);
+            Console.WriteLine("The number on this die roll is: " + dice);
             Console.ReadLine();
 
         }
