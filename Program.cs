26b46cafc8986e11ba267462773eb06c86544483
sonu1239 <ss316742@gmail.com>
2021-10-10 22:17:19 +0530

    Wellcome to Sneke and Ladder Game..!

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnekeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder game...!");
+            Console.ReadLine();
+
+        }
+    }
+}
