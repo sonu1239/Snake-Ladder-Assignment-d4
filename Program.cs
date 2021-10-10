f56afc3c5b34ae641262c05bc744021a744183d6
sonu1239 <ss316742@gmail.com>
2021-10-10 23:05:54 +0530

    Position of player in sneke Ladder g

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ namespace SnekeAndLadder
 {
     class Program
     {
+        //Constants 
+        public const int NO_PLAY = 0;
+        public const int LADDER = 1;
+        public const int SNAKE = 2;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the snake and ladder game.");
@@ -15,14 +20,36 @@ namespace SnekeAndLadder
             Console.WriteLine("Player mode:single: Starting position at 0");
             Console.WriteLine("*Start Game*");
 
+
             //Variables
             int position = 0;
 
-            Random die = new Random();
-            int dice = die.Next(1, 7);
+            Random die = new Random();     //creatting random object from random class
+            Random options = new Random();
+            int dice = die.Next(1, 7);       //simulating the die throw 
             Console.WriteLine("The number on this die roll is: " + dice);
-            Console.ReadLine();
 
+            int opt = options.Next(0, 3);    //simulating the options
+
+            //options use
+            if (opt == NO_PLAY)
+            {
+                Console.WriteLine("No play: Player in same position-- " + position);
+                Console.ReadLine();
+            }
+            else if (opt == LADDER)
+            {
+                position = position + dice;
+                Console.WriteLine("Ladder! new postion-- " + position);
+                Console.ReadLine();
+            }
+            else
+            {
+                position = position - dice;
+                Console.WriteLine("Oops,Snake! new position-- " + position);
+                Console.ReadLine();
+
+            }
         }
     }
 }
