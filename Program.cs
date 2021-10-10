538df2b0eb264ffd3b0d23924a718951b95071aa
sonu1239 <ss316742@gmail.com>
2021-10-10 22:28:23 +0530

    adding player mode to sneke &ladder g

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@ namespace SnekeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to snake and ladder game...!");
+            Console.WriteLine("Welcome to the snake and ladder game");
+            Console.WriteLine();
+            Console.WriteLine("Player mode:single: Starting position at 0");
+            Console.WriteLine("*Start Game*");
+            int position = 0;
+            int dice;
             Console.ReadLine();
 
         }
